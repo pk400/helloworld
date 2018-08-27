4e847cf712572225512654121d7a4be900f593c1
Joel Aro <joelaro.to@gmail.com>
2018-08-27 10:59:17 -0400

    Added User class

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,15 @@
-print('hello world')
+class User:
+    name = ''
+    def __init__(self, name):
+        self.name = name
+
+    def greeting(self):
+        sayhello('Hello ' + self.name + '!')
+
+def sayhello(str):
+  print(str)
+
+sayhello('hello world!')
+
+jj = User('Joel')
+jj.greeting()
